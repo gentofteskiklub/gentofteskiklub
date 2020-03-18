02625063f453eb62c0c353947d0b8cc8754c6594
carlsendk <40060660+carlsendk@users.noreply.github.com>
2020-03-18 11:47:12 +0100

    Captcha code

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -142,7 +142,7 @@ class Form extends React.Component {
           </label>
           <div
             className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+            data-sitekey="6Ld9JuIUAAAAANO_EyUK9qReD1z6a3xcbk-VULaZ"
           />
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
