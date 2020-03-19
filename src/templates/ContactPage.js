00ee0d59698585952084614bf95a742ea828103b
carlsendk <40060660+carlsendk@users.noreply.github.com>
2020-03-19 11:13:04 +0100

    Fix form

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
 import FormSimpleAjax from '../components/FormSimpleAjax'
+import FormSimple from '../components/FormSimple
 import Content from '../components/Content'
 import GoogleMap from '../components/GoogleMap'
 import Layout from '../components/Layout'
