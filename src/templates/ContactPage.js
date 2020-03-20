41cedfff571ff3999c83459b7870a5027202b513
carlsendk <40060660+carlsendk@users.noreply.github.com>
2020-03-20 13:03:55 +0100

    Back to standart form

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -56,7 +56,28 @@ export const ContactPageTemplate = ({
           </div>
         </div> 
         <div>
-        <FormSimpleAjax name="GS form" />
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+   {/* You still need to add the hidden input with the form name to your JSX form */}
+   <input type="hidden" name="form-name" value="contact" />
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
         </div>     
       </div>
     </section>
